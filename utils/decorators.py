60037dca88d7b87308226ebe9738cefec929ebbf
Ben Armstrong <synrg@debian.org>
2021-12-06 14:03:30 -0400

    Retain deprecated decorators for reference.

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,6 +1,8 @@
 """Decorator utilities."""
 from functools import wraps
 
+from ..formatters.discord import MAX_EMBED_NAME_LEN
+
 
 def make_decorator(function):
     """Make a decorator that has arguments."""
@@ -14,3 +16,18 @@ def make_decorator(function):
         return lambda wrapped_function: function(wrapped_function, *args, **kwargs)
 
     return wrap_make_decorator
+
+
+# Deprecated:
+
+
+@make_decorator
+def format_items_for_embed(function, max_len=MAX_EMBED_NAME_LEN):
+    """Format items as delimited list not exceeding Discord length limits."""
+
+    @wraps(function)
+    def wrap_format_items_for_embed(*args, **kwargs):
+        kwargs["max_len"] = max_len
+        return function(*args, **kwargs)
+
+    return wrap_format_items_for_embed
