0c32fc05f640f68b74722ddb5df5fa774ed80430
Ben Armstrong <synrg@debian.org>
2023-02-03 14:38:55 -0400

    Remove workarounds for pyinat issues 446 and 447.

--- dronefly/core/commands/__init__.py
+++ dronefly/core/commands/__init__.py
@@ -15,7 +15,7 @@ class Format(Enum):
     rich = 2
 
 
-INAT_DEFAULTS = {"locale": "en", "preferred_place_id": 1}
+INAT_DEFAULTS = {"locale": "en", "preferred_place_id": 1, "all_names": True}
 
 
 class Context:
@@ -43,43 +43,31 @@ class Commands:
         return self.parser.parse(query_str)
 
     def taxon(self, ctx: Context, *args):
-        def _pyinat_workarounds(client, taxon, **kwargs):
+        # TODO: Remove conservation status workaround once there's a solution
+        # provided by pyinat
+        def _pyinat_workarounds(taxon):
             # - https://github.com/pyinat/pyinaturalist/issues/447
             status = taxon.conservation_status
             status_name = None
             if status:
                 status_name = status.status_name
-            # - https://github.com/pyinat/pyinaturalist/issues/448
-            matched_term = taxon.matched_term
-
-            # - https://github.com/pyinat/pyinaturalist/issues/446
-            # taxon.load_full_record()
-            taxon = client.taxa(taxon.id, **kwargs)
-            return (taxon, status_name, matched_term)
+            return status_name
 
         query = self._parse(" ".join(args))
         # TODO: Handle all query clauses, not just main.terms
         # TODO: Doesn't do any ranking or filtering of results
-        # TODO: Remove workarounds for issues linked below
         main_query_str = " ".join(query.main.terms)
-        # - https://github.com/pyinat/pyinaturalist/issues/446
-        #   - all_names can be provided via default_params after #446 is fixed
-        kwargs = {"all_names": True}
 
         with self.inat_client.set_ctx(ctx) as client:
-            taxon = client.taxa.autocomplete(q=main_query_str, **kwargs).one()
+            taxon = client.taxa.autocomplete(q=main_query_str).one()
             if not taxon:
                 return "Nothing found"
-            (
-                taxon,
-                status_name,
-                matched_term,
-            ) = _pyinat_workarounds(client, taxon, **kwargs)
+            status_name = _pyinat_workarounds(taxon)
+            taxon = client.taxa.full_record(taxon)
 
         response = format_taxon(
             taxon,
             lang=INAT_DEFAULTS["locale"],
-            matched_term=matched_term,
             status_name=status_name,
             with_url=True,
         )
