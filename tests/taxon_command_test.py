2bd862b4e3ae1edf627d603a72190eb847832a1b
Ben Armstrong <synrg@debian.org>
2023-03-03 15:44:56 -0400

    Remove no longer needed skip test if old pyinat.

--- tests/taxon_command_test.py
+++ tests/taxon_command_test.py
@@ -21,8 +21,6 @@ def ctx():
 
 # TODO: Mock communication with iNatClient
 def test_taxon_with_result(cmd, ctx):
-    if not getattr(cmd.inat_client.taxa, "populate", None):
-        pytest.skip("Not yet supported by pyinaturalist: client.taxa.populate(taxon)")
     response = re.sub(r"\[[0-9,]*?\]", "[19,999,999]", cmd.taxon(ctx, "birds"))
     assert response == (
         "[Class Aves (Birds)](https://www.inaturalist.org/taxa/3) \\\nis a class with "
