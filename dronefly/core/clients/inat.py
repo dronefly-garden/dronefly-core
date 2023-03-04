a2745ab8addec4a7d45b5fdcf341e659ca85183d
Ben Armstrong <synrg@debian.org>
2023-03-04 11:28:57 -0400

    Only add home place to request if valid for it.

--- dronefly/core/clients/inat.py
+++ dronefly/core/clients/inat.py
@@ -1,19 +1,33 @@
 """Module to access iNaturalist API."""
 from contextlib import contextmanager
+from typing import Optional
 
 from pyinaturalist import iNatClient as pyiNatClient
+from pyinaturalist.constants import RequestParams
+from pyinaturalist.request_params import get_valid_kwargs
 
 
 class iNatClient(pyiNatClient):
     """iNat client based on pyinaturalist."""
 
-    def add_client_settings(self, *args, **kwargs):
-        request_kwargs = super().add_client_settings(*args, **kwargs)
-        user = self.ctx.author
-        if user and user.inat_place_id:
-            request_kwargs["preferred_place_id"] = user.inat_place_id
+    def add_client_settings(
+        self,
+        request_function,
+        kwargs: Optional[RequestParams] = None,
+        auth: bool = False,
+    ):
+        _kwargs = super().add_client_settings(request_function, kwargs, auth)
 
-        return request_kwargs
+        user = self.ctx and self.ctx.author
+        preferred_place_id = user and user.inat_place_id
+        if preferred_place_id:
+            user_kwargs = get_valid_kwargs(
+                request_function, {"preferred_place_id": preferred_place_id}
+            )
+            for k, v in user_kwargs.items():
+                _kwargs.setdefault(k, v)
+
+        return _kwargs
 
     @contextmanager
     def set_ctx(self, ctx):
