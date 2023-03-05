1d26986cf3b1f602a56d1f2d47caf059d223300f
Ben Armstrong <synrg@debian.org>
2023-03-05 09:27:41 -0400

    client: Simplified adding context user params.

--- dronefly/core/clients/inat.py
+++ dronefly/core/clients/inat.py
@@ -1,10 +1,10 @@
 """Module to access iNaturalist API."""
 from contextlib import contextmanager
+from inspect import signature
 from typing import Optional
 
 from pyinaturalist import iNatClient as pyiNatClient
 from pyinaturalist.constants import RequestParams
-from pyinaturalist.request_params import get_valid_kwargs
 
 
 class iNatClient(pyiNatClient):
@@ -19,13 +19,12 @@ class iNatClient(pyiNatClient):
         _kwargs = super().add_client_settings(request_function, kwargs, auth)
 
         user = self.ctx and self.ctx.author
-        preferred_place_id = user and user.inat_place_id
-        if preferred_place_id:
-            user_kwargs = get_valid_kwargs(
-                request_function, {"preferred_place_id": preferred_place_id}
-            )
-            for k, v in user_kwargs.items():
-                _kwargs.setdefault(k, v)
+        if (
+            user
+            and user.inat_place_id
+            and "preferred_place_id" in signature(request_function).parameters
+        ):
+            _kwargs.setdefault("preferred_place_id", user.inat_place_id)
 
         return _kwargs
 
