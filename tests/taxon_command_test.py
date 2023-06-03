6c6d172736b022ca562ec3168e6ac072d994b3c6
Ben Armstrong <synrg@debian.org>
2023-06-03 13:01:46 -0300

    Non-rich test no longer expects rich line breaks.

--- tests/taxon_command_test.py
+++ tests/taxon_command_test.py
@@ -23,7 +23,7 @@ def ctx():
 def test_taxon_with_result(cmd, ctx):
     response = re.sub(r"\[[0-9,]*?\]", "[19,999,999]", cmd.taxon(ctx, "birds"))
     assert response == (
-        "[Class Aves (Birds)](https://www.inaturalist.org/taxa/3) \\\nis a class with "
+        "[Class Aves (Birds)](https://www.inaturalist.org/taxa/3)\nis a class with "
         "[19,999,999](https://www.inaturalist.org/observations?taxon_id=3) observations in: "
         "\n> **Animalia** > \n> **Chordata** > Vertebrata"
     )
