da76067c8ef7f97feec1c87e5850bc82c2dffafd
Ben Armstrong <synrg@debian.org>
2026-08-05 09:43:00 -0300

    Fix selected obs link detection

--- dronefly/core/parsers/url.py
+++ dronefly/core/parsers/url.py
@@ -48,9 +48,9 @@ PAT_OBS_LINK = re.compile(_PAT_OBS_LINK, re.I)
 QUERY_PAT = r"\??(?:&?[^\n=& ]*=[^\n=& ]*)*"
 _OBS_QUERY_PAT = r"(?P<url>" + WWW_URL_PAT + r"/observations" + QUERY_PAT + ")"
 PAT_OBS_QUERY = re.compile(_OBS_QUERY_PAT)
-# - the ">" character is markdown for block-quote, but only at the start of a
-#   line, so in the middle of a line we use it to indicate a selected entry
-_SELECTED_OBS_LINK_PAT = r"^(?!> ).*?>.*?\[.*?\]\(" + _PAT_OBS_LINK + r"\)"
+# - the ">" character is markdown for block-quote, but only when followed by a
+#   blank, so in the middle of a line we use it to indicate a selected entry
+_SELECTED_OBS_LINK_PAT = r">[^ ].*?\[.*?\]\(" + _PAT_OBS_LINK + r"\)"
 PAT_SELECTED_OBS_LINK = re.compile(_SELECTED_OBS_LINK_PAT, re.MULTILINE)
 MARKDOWN_LINK = re.compile(r"\[.*?\]\((?P<url>.*?)\)")
 
