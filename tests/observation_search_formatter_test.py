ddc6a9a38f08aa5e12935dfa1ef271f9e5fb34ed
Ben Armstrong <synrg@debian.org>
2026-08-08 10:20:44 -0300

    obs search format: fix expectation for test

--- tests/observation_search_formatter_test.py
+++ tests/observation_search_formatter_test.py
@@ -1,3 +1,4 @@
+# flake8: noqa
 import pytest
 from unittest.mock import Mock
 from dronefly.core.formatters.generic import ObservationSearchFormatter
@@ -70,20 +71,30 @@ def test_initialization():
 
 
 def test_format(mock_source):
-    formatter = ObservationSearchFormatter()
+    formatter = ObservationSearchFormatter(
+        with_summaries=True,
+    )
     formatter.source = mock_source
     page = mock_source.entries[:10]
     formatted_page = formatter.format(page, 1, 0)
-    expected_output = """[Search: Observations of taxa by test_user](https://www.inaturalist.org/observations?user=1)
-
-`Jul-2025`>**__[*Genus1 species00*](https://www.inaturalist.org/observations/1)__**
-`Jul-2025` [*Genus1 species01*](https://www.inaturalist.org/observations/2)
-`Jul-2025` [*Genus1 species02*](https://www.inaturalist.org/observations/3)
-`Jul-2025` [*Genus1 species03*](https://www.inaturalist.org/observations/4)
-`Jul-2025` [*Genus1 species04*](https://www.inaturalist.org/observations/5)
-`Jul-2025` [*Genus1 species05*](https://www.inaturalist.org/observations/6)
-`Jul-2025` [*Genus1 species06*](https://www.inaturalist.org/observations/7)
-`Jul-2025` [*Genus1 species07*](https://www.inaturalist.org/observations/8)
-`Jul-2025` [*Genus1 species08*](https://www.inaturalist.org/observations/9)
-`Jul-2025` [*Genus1 species09*](https://www.inaturalist.org/observations/10)"""  # noqa: E501
+    expected_title = (
+        "[Search: Observations of taxa by test_user]"
+        "(https://www.inaturalist.org/observations?user=1)\n"
+    )
+    en = "\N{EN SPACE}"
+    indent = f"\N{ZERO WIDTH SPACE}{en}"
+    icon = "\N{REPLACEMENT CHARACTER}"
+    sel = "\N{BLACK RIGHT-POINTING SMALL TRIANGLE}"
+    expected_link = lambda n: (
+        f"[*Genus1 species{str(n).zfill(2)}*]"
+        f"(https://www.inaturalist.org/observations/{n + 1})"
+    )
+    expected_summary = f"{en}`Jul-2025                        `{en}{icon}"
+    expected_obs = lambda n: "\n".join(
+        (f"{indent}{expected_link(n)}", expected_summary)
+    )
+    expected_sel = "\n".join((f"{sel}**__{expected_link(0)}__**", expected_summary))
+    expected_output = "\n".join(
+        (expected_title, expected_sel, *(expected_obs(n) for n in range(1, 10)))
+    )
     assert formatted_page == expected_output
