84ded7ffc75cf482a05cf6b78f3ca8fe293d3971
Ben Armstrong <synrg@debian.org>
2026-08-09 05:41:24 -0300

    obs search: add custom paginator with annotation support
    - we did not correctly implement the annotation callback, instead
      passing it through on the API request
    - fortunately, the API simply ignored the wrong
       annotation_callback parameter we were sending to it
    - our custom ObservationPaginator is based on Paginator instead
       of ID range paginator used upstream

--- dronefly/core/controllers/inat/observation_controller.py
+++ dronefly/core/controllers/inat/observation_controller.py
@@ -1,10 +1,12 @@
+from collections.abc import Callable
+
 from pyinaturalist.constants import V1_OBS_ORDER_BY_PROPERTIES
 from pyinaturalist.controllers import (
     ObservationController as pyiNatObservationController,
 )
 from pyinaturalist.docs import copy_doc_signature
 from pyinaturalist.docs import templates as docs
-from pyinaturalist.models import Observation
+from pyinaturalist.models import Annotation, BaseModel, Observation
 from pyinaturalist.request_params import validate_multiple_choice_param
 from pyinaturalist.v1 import get_observations
 
@@ -19,10 +21,34 @@ class ObservationController(pyiNatObservationController):
         )
         params = self.client.add_defaults(get_observations, params)
 
-        return Paginator(
-            get_observations,
-            Observation,
+        return ObservationPaginator(
             loop=self.client.loop,
             annotation_callback=self.client.annotations.lookup,
             **params,
         )
+
+
+class ObservationPaginator(Paginator):
+    """Paginate through observation results
+
+    This also optionally fills in missing annotation information for each observation, using results
+    from ``GET /controlled_terms``.
+    """
+
+    def __init__(
+        self,
+        request_function: Callable = get_observations,
+        model: BaseModel = Observation,
+        *args,
+        annotation_callback: Callable[[list[Annotation]], list[Annotation]],
+        **kwargs,
+    ):
+        super().__init__(request_function, model, *args, **kwargs)
+        self.annotation_callback = annotation_callback
+
+    def next_page(self) -> list[Observation]:
+        observations = super().next_page()
+        # Use cached controlled_terms lookup to fill in missing annotation details
+        for obs in observations:
+            obs.annotations = self.annotation_callback(obs.annotations)
+        return observations
